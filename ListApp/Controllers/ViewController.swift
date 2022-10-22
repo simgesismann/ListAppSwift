f917faf49d4b993c059647a5651d3f220f371ad9
simgesismann <62858275+simgesismann@users.noreply.github.com>
2022-10-23 00:22:56 +0300

    table view, delegate,datasource are added, array is obtained, app shows table view with data in array

--- ListApp/Controllers/ViewController.swift
+++ ListApp/Controllers/ViewController.swift
@@ -7,13 +7,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let data = ["a","b","c","d","e"]
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "defaultCell", for: indexPath)
+        cell.textLabel?.text = data[indexPath.row]
+        return cell
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
 
 }
 
